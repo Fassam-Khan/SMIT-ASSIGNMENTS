8bbf7306688b07d8291334b7abc8c653a2571cf3
Fassam Khan <muhammadfassam465@gmail.com>
2025-11-21 16:30:28 +0500

    add assignment 5

---
+++ Arrays/app.js
@@ -0,0 +1,6 @@
+var cities = ["Karachi","Lahore","Islamabad","quetta"]
+cities.push("Peashawar");
+cities.pop();
+cities.shift()
+
+console.log(cities)
---
+++ JS Assignment 5/app.js
@@ -0,0 +1,74 @@
+var a = 3;
+var b = 5;
+console.log("Sum = ",a+b);
+console.log("Substraction = ",a-b);
+console.log("Multiplication = ",a*b);
+console.log("Division = ",a/b);
+console.log("Modulus = ",a%b);
+
+
+var ticket = 600;
+console.log("Total cost to buy 5 tickets to a movie is ",ticket*5 + "PKR");
+
+console.log("Table of 4");
+console.log("4 x 1 = ",4*1)
+console.log("4 x 2 = ",4*2)
+console.log("4 x 3 = ",4*3)
+console.log("4 x 4 = ",4*4)
+console.log("4 x 5 = ",4*5)
+console.log("4 x 6 = ",4*6)
+console.log("4 x 7 = ",4*7)
+console.log("4 x 8 = ",4*8)
+console.log("4 x 9 = ",4*9)
+console.log("4 x 10 = ",4*10)
+
+var temprature = +prompt("Enter temprature in celcius");
+F = (temprature * 9/5) +32;
+console.log(temprature + "C" , "is", F + "F")
+
+var temprature = +prompt("Enter temprature in farhenheit");
+C = (temprature - 32) *5/9;
+console.log(temprature + "F" , "is", C + "C")
+
+var item_1_price = 650;
+var item_2_price = 100;
+var item_1_quantity = 3;
+var item_2_quantity = 7;
+var shippingCharges = 100;
+
+console.log("Price of item 1 is ", item_1_price);
+console.log("Quantity of item 1 is  ", item_1_quantity);
+console.log("Price of item 2 is ", item_2_price);
+console.log("Quantity of item 2 is  ", item_2_quantity);
+console.log("Shipping Charges ", shippingCharges);
+
+console.log("Total cost of your order is ", item_1_price*item_1_quantity + item_2_price*item_2_quantity + shippingCharges);
+
+var mark1 = 700;
+var mark2 = 104;
+obtainMarks = mark1+ mark2
+percent = obtainMarks / 980 * 100;
+console.log("Total Marks: 980");
+console.log("Marks obtained: ",obtainMarks);
+console.log("Percentage:",percent);
+
+
+var usDollar = 10;
+var ryal = 25;
+var US = 104.80;
+var SaudiaRyal = 28;
+var totalRupees = (usDollar * US) + (ryal * SaudiaRyal);
+console.log("Total currency in PKR: ", totalRupees);
+
+var num = 10;
+var result = num +5 * 10/2;
+console.log(result)
+
+var currentYear = 2025;
+var birthYear = 2007;
+var age = currentYear  - birthYear;
+console.log("Your age is ",age);
+
+
+
+
