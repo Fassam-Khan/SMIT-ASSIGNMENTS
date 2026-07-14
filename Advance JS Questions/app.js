8dc14d0eb251aebf73c7585d52a3bd8ae359a90c
Fassam-Khan <muhammadfassam465@gmail.com>
2026-07-14 13:01:22 +0500

    ass

---
+++ Advance JS Questions/app.js
@@ -0,0 +1,78 @@
+// console.log("Js running >>>");
+
+// let product = {
+//     name : "Iphone 17",
+//     color : "black",
+//     ram : "8 GB",
+//     price : 45000
+// }
+
+
+// function showPrice(name2) {
+//     console.log(this.name);    
+//         console.log(this[name2]);    
+
+// }
+
+
+// console.log("Product >>", product);
+
+// showPrice.call(product, "name" )
+
+// Call function may argument may "name" aisay argument may pass hota hay 
+
+// let product = {
+//     name : "Iphone 17",
+//     color : "black",
+//     ram : "8 GB",
+//     price : 45000
+// }
+
+
+// function showPrice(name2) {
+//     console.log(this.name);    
+//         console.log(this[name2]);    
+
+// }
+
+
+// console.log("Product >>", product);
+
+// showPrice.apply(product, ["name"] )
+
+// Apply  function may argument may ["name"] array  argument may pass hota hay 
+
+
+// let product = {
+//     name : "Iphone 17",
+//     color : "black",
+//     ram : "8 GB",
+//     price : 45000
+// }
+
+// let product2 = {
+//     name : "Iphone 18",
+//     color : "white",
+//     ram : "4 GB",
+//     price : 85000
+// }
+
+
+// function showPrice([...name2]) {
+//         console.log(this[name2]);    
+
+// }
+
+
+// console.log("Product >>", product);
+
+// let bindMethod =  showPrice.bind(product2,["name"] )
+
+// console.log(bindMethod());
+
+// // Bind  function may argument may return milta hay 
+
+
+
+
+
